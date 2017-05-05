bb93604632ae8618987fdcbc9a89e95166412fcb
Trevor Sherwood <sherwoodwt@gmail.com>
2017-05-05 02:51:55 -0700

    Added build file as notes to shorten jenkins server build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install hugo -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+sudo pip install awscli --user
+sudo apt-get install awscli
