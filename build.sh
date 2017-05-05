a74571f5e0602b77d91c9c8efd8e04e731bb9ff3
Trevor Sherwood <sherwoodwt@gmail.com>
2017-05-05 03:00:28 -0700

    modified build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
-sudo apt-get install python -y
-sudo apt-get install python-pip -y
-sudo pip install awscli --user
-sudo apt-get install awscli
+sudo apt-get install python3 -y
+# sudo apt-get install python-pip -y
+# sudo pip install awscli --user
+# sudo apt-get install awscli
