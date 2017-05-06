7bd22a271f78bf539885f0365545126f61462dfc
Trevor Sherwood <sherwoodwt@gmail.com>
2017-05-06 15:43:31 -0700

    Added build options

--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install hugo -y
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins -y
-sudo apt-get install python3 -y
-# sudo apt-get install python-pip -y
-# sudo pip install awscli --user
-# sudo apt-get install awscli
+hugo --theme=themes/dimension --buildDrafts
+aws s3 sync public/ s3://the-monties-epk --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
